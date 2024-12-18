287214236aa305abbb7be9fd200625743b1b7429
Sukhoruchko Oleh <osukhoruchko@gmail.com>
2024-12-18 17:19:03 +0200

    Create full functional linked list

---
+++ main.rb
@@ -0,0 +1,11 @@
+require_relative 'lib/linkedlist'
+
+list = LinkedList.new
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
+list.pop
+puts list
